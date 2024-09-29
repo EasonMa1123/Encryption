37327584d4d28193a849b533a28992c7bb84e414
EasonMa1123 <yoyomam1123@gmail.com>
2024-09-29 22:50:21 +0100

    Add function: upload txt file
    allow user upload txt file to encrypt and Decrypt

--- static/script.js
+++ static/script.js
@@ -10,29 +10,59 @@ y.style.display = "block";
 z.style.display = "None";
 
 function encryptMessage() {
+    var message_file = document.getElementById('plain_text_message_file').files[0];
     var message = document.getElementById('message').value;
     var password = document.getElementById('password_to_encrypte').value;
-    if (message != "" ){
-        $.post("/encrypt", { message: message,password:password }, function(data) {
+    if (message_file) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var fileContent = event.target.result;
+            encryptingMessage(fileContent,password);
+        };
+        reader.readAsText(message_file);
+    } else if (message !== "") {
+        encryptingMessage(message, password);
+    } else {
+        alert("Invalid Message");
+    }
+}
+
+
+function encryptingMessage(message, password) {
+    $.post("/encrypt", { message: message,password:password }, function(data) {
         $('#encrypted_message').text(data.encrypted_message);
         $('#key').text(data.key);
-    });} else {
-        alert("Invalid Message")
-    }
+    });
 }
 
 function decryptMessage() {
+    var message_file = document.getElementById('decrypt_message_file').files[0];
     var message = $('#decrypt_message').val();
     var key = $('#decrypt_key').val();
     var password = document.getElementById('password_to_decrypte').value;
-    if (message != ""){
-        $.post("/decrypt", { message: message, key: key,password:password }, function(data) {
-        $('#decrypted_message').text(data.decrypted_message);
-    });} else {
-        alert("Invalid Message")
 
+    if (message_file) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var fileContent = event.target.result;
+            decryptingMessage(fileContent, key, password);
+        };
+        reader.readAsText(message_file);
+    } else if (message !== "") {
+        decryptingMessage(message, key, password);
+    } else {
+        alert("Invalid Message");
     }
 }
+
+
+
+function decryptingMessage(message, key, password) {
+    $.post("/decrypt", { message: message, key: key, password: password }, function(data) {
+        $('#decrypted_message').text(data.decrypted_message);
+    });
+}
+
 function show_encryption(){
     var w = document.getElementById("Encryption_button");
     var x = document.getElementById("Decryption_button");
@@ -68,11 +98,18 @@ function Copy_text(ID){
 
 function Create_txt_file (ID){
     const message= document.getElementById(ID).textContent;
-    const link = document.createElement("a");
-    const content = message;
-    const file = new Blob([content], { type: 'text/plain' });
-    link.href = URL.createObjectURL(file);
-    link.download = "sample.txt";
-    link.click();
-    URL.revokeObjectURL(link.href);
+    if (message != ""){
+        const link = document.createElement("a");
+        const content = message;
+        const file = new Blob([content], { type: 'text/plain' });
+        link.href = URL.createObjectURL(file);
+        link.download = "sample.txt";
+        link.click();
+        URL.revokeObjectURL(link.href);
+    } else {
+        alert("No Text to download!")
+    }
 }
+
+
+
