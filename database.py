bd9e70e45e87bf5f500f509b5a4fc99b8d6a0016
EasonMa2311 <19Ema@haz5d.com>
2024-10-10 15:33:14 +0000

    Add insert function

--- database.py
+++ database.py
@@ -1,11 +1,26 @@
 import sqlite3
 
-def create_check_database():
-    DataBase = sqlite3.connect("message_encrypted.db")
-    cc = DataBase.cursor()
-    cc.execute("CREATE TABLE IF NOT EXISTS message (id,encrypted_message,key)")
-    DataBase.commit()
-    
+class DataRecord:
+    def __init__(self):
+        # Connect to the database
+        self.DataBase = sqlite3.connect("message_encrypted.db")
+        self.cc = self.DataBase.cursor()
+        # Create the table with data types for each column
+        self.cc.execute("""
+            CREATE TABLE IF NOT EXISTS message (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                encrypted_message TEXT,
+                key TEXT
+            )
+        """)
+        self.DataBase.commit()
 
+    def insert_data(self, message, key):
+        self.cc.execute('SELECT COUNT(id) FROM message')
+        id = self.cc.fetchall()[0][0]
+        self.cc.execute('INSERT INTO message (id , encrypted_message, key) VALUES (? , ?, ?);', (id,message, key))
+        self.DataBase.commit()
 
-create_check_database() 
+# Create an instance of DataRecord and insert data
+data = DataRecord()
+data.insert_data('hi', '1')
