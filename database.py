ffe4b8eed659c4496c24b9e9f8c9b3a6d141cfca
EasonMa2311 <19Ema@haz5d.com>
2024-10-10 13:30:31 +0000

    add function: create_check_database

---
+++ database.py
@@ -0,0 +1,12 @@
+import mysql.connector
+import sqlite3
+
+def create_check_database():
+    DataBase = sqlite3.connect("message_encrypted.db")
+    cc = DataBase.cursor()
+    cc.execute("CREATE TABLE IF NOT EXISTS message (id,encrypted_message,key)")
+    DataBase.commit()
+    
+
+
+create_check_database() 
