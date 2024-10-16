313c1b9b8350b0523b43c92c35c85a8488530c0b
EasonMa2311 <19Ema@haz5d.com>
2024-10-16 10:48:35 +0000

    warn user if password is too short

--- static/home_event_script.js
+++ static/home_event_script.js
@@ -1,12 +1,14 @@
 var Password_input = document.getElementById("signup-Password");
 var Password_input_value = Password_input.value;
-var password_warning = document.getElementById("Password-warning");
+var password_warning_1 = document.getElementById("Password-warning-1");
+var password_warning_2 = document.getElementById("Password-warning-2");
+
 
 Password_input.onkeyup = function(){
-    
-    
-    if (Password_input_value.length < 8 ){
-        password_warning.innerHTML = "Password must longer than 8";
-    }
 
+    if (Password_input_value.length < 8 ){
+        password_warning_1.innerHTML = "Password must longer than 8";
+    } else if (Password_input_value.length > 0){
+        password_warning_1.innerHTML = "";
+    }
 }
