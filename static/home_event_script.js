af887e4ddf07a2ba4319d00278f13018ebbf9959
EasonMa1123 <yoyomam1123@gmail.com>
2024-10-16 21:02:20 +0100

    fix error 2

--- static/home_event_script.js
+++ static/home_event_script.js
@@ -1,19 +1,24 @@
 var Password_input = document.getElementById("signup-Password");
-var Password_input_value = Password_input.value;
-var password_warning_1 = document.getElementById("Password-warning-1");
-var password_warning_2 = document.getElementById("Password-warning-2");
 
 
 Password_input.onkeyup = function(){
+    var Password_input_value = Password_input.value;
+    var password_warning_1 = document.getElementById("Password-warning-1");
+    var password_warning_2 = document.getElementById("Password-warning-2");
+
 
     if (Password_input_value.length < 8 ){
         password_warning_1.innerHTML = "Password must longer than 8";
     } else if (Password_input_value.length > 0){
         password_warning_1.innerHTML = "";
+    } else if (Password_input_value.length > 40){
+        password_warning_1.innerHTML = "Too long!";
     }
-    $.post('/password_strength',{Password:password},function(data){
-        if (data.score){
-
+    $.post('/password_strength',{Password:Password_input_value},function(data){
+        if (data.score < 20){
+            password_warning_2.innerHTML = "Password is not strong enough"
+        } else{
+            password_warning_2.innerHTML = ""
         }
     })
 }
