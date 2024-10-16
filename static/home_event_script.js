d37231badc23997635895e6454cadf2cf38f2285
EasonMa2311 <19Ema@haz5d.com>
2024-10-16 11:24:41 +0000

    real warning

--- static/home_event_script.js
+++ static/home_event_script.js
@@ -1,14 +1,16 @@
-var Password_input = document.getElementById("signup-Password");
-var Password_input_value = Password_input.value;
-var password_warning_1 = document.getElementById("Password-warning-1");
-var password_warning_2 = document.getElementById("Password-warning-2");
+const Password_input = document.getElementById("signup-Password");
 
 
 Password_input.onkeyup = function(){
+    
+    const Password_input_value = document.getElementById('signup-Password').value;
+    var password_warning_1 = document.getElementById("Password-warning-1");
+    var password_warning_2 = document.getElementById("Password-warning-2");
 
-    if (Password_input_value.length < 8 ){
+    if (Password_input_value.length <=8 ){
         password_warning_1.innerHTML = "Password must longer than 8";
-    } else if (Password_input_value.length > 0){
+    } 
+    if(Password_input_value.length > 8){
         password_warning_1.innerHTML = "";
     }
 }
