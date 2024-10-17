b2f54e812cf9b25524b21a3abdbe359740b27dc0
EasonMa2311 <19Ema@haz5d.com>
2024-10-17 08:37:22 +0000

    add check comfirm password

--- static/home_event_script.js
+++ static/home_event_script.js
@@ -1,4 +1,5 @@
 const Password_input = document.getElementById("signup-Password");
+const comfirmPassword_input = document.getElementById("ConfirmPassword");
 
 
 Password_input.onkeyup = function(){
@@ -8,10 +9,10 @@ Password_input.onkeyup = function(){
     var password_warning_2 = document.getElementById("Password-warning-2");
 
 
-    if (Password_input_value.length <=8 ){
-        password_warning_1.innerHTML = "Password must longer than 8";
+    if (Password_input_value.length <8 ){
+        password_warning_1.innerHTML = "Password must longer than 7";
     } 
-    if(Password_input_value.length > 8){
+    if(Password_input_value.length >= 8){
         password_warning_1.innerHTML = "";
     } else if (Password_input_value.length > 40){
         password_warning_1.innerHTML = "Too long!";
@@ -23,4 +24,18 @@ Password_input.onkeyup = function(){
             password_warning_2.innerHTML = ""
         }
     })
+}
+
+comfirmPassword_input.onkeyup = function(){
+    var Password = document.getElementById('signup-Password').value;
+    var comfirmPassword_input_value = comfirmPassword_input.value;
+    var comfirmPassword_input_warning = document.getElementById('ConfirmPassword-warning');
+    if (comfirmPassword_input_value.length>(Password.length/2)){
+        if (comfirmPassword_input_value != Password){
+            
+            comfirmPassword_input_warning.innerHTML = "Incorrect comfirming password";
+        } else {
+            comfirmPassword_input_warning.innerHTML = "";
+        }
+    }
 }
