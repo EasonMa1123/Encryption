28e54d449ec9dae93cd4fabe36031bd808cbdb47
EasonMa2311 <19Ema@haz5d.com>
2024-11-05 13:03:49 +0000

    add space

--- encryption_testing.py
+++ encryption_testing.py
@@ -7,32 +7,43 @@ slot = "abcdefghijklmnMNOPQRSTUVWXYZopqrstuvwxyzABCDEFGHIJKL!@#$%^&*()_+-=[]{|\;
 password = "123"
 
 def check_encryption():
+    
     incorrect_counter = 0
     invalid_counter = 0
-    total_test_time = 100
+    total_test_time = 1000
+    text_length = 100000
     en_run_times = []
     de_run_times = []
     
     for i in range(total_test_time):
+
         plain_text = ""
-        for time in range(1000000):
+        for time in range(text_length):
             plain_text += slot[random.randrange(len(slot))]
+
         encryption_start_time = Time.time()
         cipyer_message,key = encryption.encryption(plain_text,password)
         encryption_end_time = Time.time()
         en_run_times.append(encryption_end_time-encryption_start_time)
+
         decryption_start_time = Time.time()
         return_message = encryption.unencryption(cipyer_message,key,password)
         decryption_end_time = Time.time()
         de_run_times.append(decryption_end_time-decryption_start_time)
+
         if return_message != plain_text and return_message!= "Invalid Password,unable to decrypte":
             incorrect_counter+=1
             print(f'Incorrect encryption/decryption: Plain text: {plain_text},key: {key},return message: {return_message}')
+
         elif return_message == "Invalid Password,unable to decrypte":
             print("Invalid Password,unable to decrypte")
             invalid_counter +=1
+
+
         if i%100 == 0:
             print(f'testing number: {i}')
+
+
     print(f'total Incorrect: {incorrect_counter},total invalid: {invalid_counter}\n In% : Incorrect: {incorrect_counter/total_test_time}, invalid: {invalid_counter/total_test_time}\nAverage encrytption time:{sum(en_run_times)/len(en_run_times)}\nAverage decryption time:{sum(de_run_times)/len(de_run_times)}')
 
 check_encryption()
