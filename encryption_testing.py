6efaa032a5c94f6caf7bf34e11346886a22cdd51
EasonMa1123 <yoyomam1123@gmail.com>
2024-12-10 22:46:08 +0000

    fix testing error

--- encryption_testing.py
+++ encryption_testing.py
@@ -3,15 +3,16 @@ import random
 import time as Time
 import matplotlib.pyplot as graph
 
-encryption = Encrytion()
-slot = "abcdefghijklmnMNOPQRSTUVWXYZopqrstuvwxyzABCDEFGHIJKL!@#$%^&*()_+-=[]{|\;}:',./<>?`~1234567890 "
+
+
+slot = "ABCDEFGHI@#$%^&*()_+-=[]{|\;}:',./<>?`~JKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!1234567890 "
 password = "123"
 
-def check_encryption(text_length):
-    
+def check_encryption(text_length,split_amount):
+    encryption = Encrytion(split_amount)
     incorrect_counter = 0
     invalid_counter = 0
-    total_test_time = 3
+    total_test_time = 1
     
     en_run_times = []
     de_run_times = []
@@ -20,7 +21,7 @@ def check_encryption(text_length):
 
         plain_text = ""
         for time in range(text_length):
-            plain_text += slot[random.randrange(len(slot))]
+            plain_text += str(slot[random.randrange(len(slot))])
 
         encryption_start_time = Time.time()
         cipyer_message,key = encryption.encryption(plain_text,password)
@@ -37,11 +38,19 @@ def check_encryption(text_length):
         if return_message == "Invalid Password,unable to decrypte":
             print("Invalid Password,unable to decrypte")
             invalid_counter +=1
+
         
-        elif return_message != plain_text :
+        
+        elif str(plain_text).strip() != str(return_message).strip():
             incorrect_counter+=1
-            print(f'Incorrect encryption/decryption: Plain text: {plain_text}')
-            print(f'\n\n\nkey: {key},\n\n\nreturn message: {return_message}')
+            if len(return_message)-1 != len(plain_text):
+                print(f'Different Length {len(return_message)-1} {len(plain_text)}')
+            else:
+                for i, (char_a, char_b) in enumerate(zip(plain_text, return_message)):
+                    if char_a != char_b:
+                        print(f"Difference at index {i}: {char_a!r} != {char_b!r}")
+            print(f'Incorrect encryption/decryption: Plain text: \n{plain_text}')
+            print(f'\nkey: {key},\nreturn message: \n{return_message}\n\n')
 
 
         if i%100 == 0:
@@ -53,16 +62,24 @@ def check_encryption(text_length):
 
 
 def main():
-    Enc_times = []
-    dec_times = []
-    for i in range(1,7):
-        print(f'10 to the power {i}')
-        Enc_time,dec_time = check_encryption(10**i)
-        Enc_times.append(Enc_time)
-        dec_times.append(dec_time)
+    max_tiral_num = 8
+    for j in range(1,max_tiral_num):
+        Enc_times = []
+        dec_times = []
+        for i in range(1,max_tiral_num):
+            print(f'10 to the power {i} for string, 10 to the power of {j} for split amount')
+            Enc_time,dec_time = check_encryption(10**i,10**j)
+            Enc_label = f'split amount: 10^{j} ENC'
+            DEC_label = f'split amount: 10^{j} DEC'
+            Enc_times.append(Enc_time)
+            dec_times.append(dec_time)
 
-    graph.plot(Enc_times)
-    graph.plot(dec_times)
+        graph.plot(Enc_times,label=str(Enc_label))
+        graph.plot(dec_times,label=str(DEC_label))
+    graph.xlabel("10^x string length")
+    graph.ylabel("time(s)")
+    
+    graph.legend()
     graph.show()
 
 main()
