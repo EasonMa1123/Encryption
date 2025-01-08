76b7836f32cdf07f6700748503e6a7de0859c728
EasonMa1123 <yoyomam1123@gmail.com>
2025-01-08 17:15:45 +0000

    create basic email sent

--- static/home.js
+++ static/home.js
@@ -15,24 +15,35 @@ function submit_new_user_data(){
     let Username = document.getElementById("signup-Username").value
     let Password = document.getElementById("signup-Password").value
     let Confirm_password = document.getElementById("ConfirmPassword").value
+    const Confirm_email = document.getElementById("signup-email").value
 
+
+    alert("e")
     if (Username == "") {
         alert("please enter a username")
     }else if(Password == ""){
         alert("Please enter a password")
     }else if (Password != Confirm_password){
         alert("Invalid Password confirmation!")
-    } else{
-        $.post('/insertNewUser',{ userName: Username, Password:Password }, function(data) {
-            if (data.Feedback == "Invalid Username,This Username had been used "){
-                alert("Invalid Username,This Username had been used ")
-            } else {
-                alert("Vaild SignUp,please login to enter!")
-            }
-            
-    });}
+    }else{
+
+        $.post('/email_verification',{Email:Confirm_email}, function(data) {
+
+            let user_code = prompt("Email sent,please enter the code")
+            if (user_code == data.code){
+                $.post('/insertNewUser',{ userName: Username, Password:Password }, function(data) {
+                    if (data.Feedback == "Invalid Username,This Username had been used "){
+                        alert("Invalid Username,This Username had been used ")
+                    } else {
+                        alert("Vaild Signup,please login to enter!")}});
+                }else{alert("Invalid Code,please submit again!")}
+        }
+    )}
 
 }
+
+    
+
 
 function login(){
     let Username = document.getElementById("login-Username").value
