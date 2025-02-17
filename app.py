e37178b5c59d20a927dfe9bd7df197a03c1eda11
EasonMa1123 <yoyomam1123@gmail.com>
2025-02-17 10:07:41 +0000

    update for google run

--- app.py
+++ app.py
@@ -147,6 +147,6 @@ def email_verification():
     return jsonify({"Code":code})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8080)
+    app.run(port=8080,allow_unsafe_werkzeug=True)
 
 
