452a03bd8cb006cfae154913d2a5f6d8543c2346
EasonMa2311 <19Ema@haz5d.com>
2025-04-02 08:36:43 +0000

    add to pypi

--- tests/test_Encryption.py
+++
@@ -1,5 +0,0 @@
-from mypythonlib.encryption_V2 import Encrytion
-
-def test_encryption():
-    cipher_text,password = Encrytion().encryption("this is a message","123") 
-    assert Encrytion().unencryption(cipher_text,password,"123") == "this is a message"
